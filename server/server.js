4bdea95e082b06102317dcb0667b6242246a2854
Keenan Faulkner <keenan.r.faulkner@gmail.com>
2022-06-20 17:33:40 -0600

    Added rollbar APM

--- server/server.js
+++ server/server.js
@@ -9,11 +9,24 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '987c7917deec4b63b2c22d7fddf0fae5',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.post(`/seed`, seed);
 app.post(`/emails`, addEmail);
 app.put(`/emails`, removeEmail);
+
+app.get('/dne', (req, res) => {
+    callNonExistentMethod();
+    res.status(200).send('GET successful at /dne').catch(err => rollbar.error(err));
+});
 
 const port = process.env.PORT || 4005;
 app.listen(port, () => {
